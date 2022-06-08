89139c7b4a7db4481f4d287a9c9cc5852dcce197
zhakevich <zhakevichv@ya.ru>
2022-06-09 01:18:30 +0300

    fix: fix name

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 // import '../style.css';
-import '../../node_modules/focus-visible/dist/focus-visible';
+// import '../../node_modules/focus-visible/dist/focus-visible';
 
 import '../scss/main.scss';
 import '../index.html';
